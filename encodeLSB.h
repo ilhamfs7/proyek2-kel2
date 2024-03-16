403b7f7dd835ee37b53395a1bf8bc830762136c5
pace <143985393+ilhamfs7@users.noreply.github.com>
2024-03-16 16:43:19 +0700

    Update encodeLSB.h

--- encodeLSB.h
+++ encodeLSB.h
@@ -1,6 +1,7 @@
 #ifndef ENCODELSB_H
 #define ENCODELSB_H
 
+void readBMPHeader(FILE *fin, unsigned char header[54])
 void encodeLSB(const char *inputimg, const char *outputimg, const char *pesan);
 
 #endif
