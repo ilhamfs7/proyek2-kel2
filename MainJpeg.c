4173abda9f581e1874df35cac0dc39081d52d51d
ninoerico <158866190+ninoerico@users.noreply.github.com>
2024-05-06 08:45:33 +0700

    Update MainJpeg.c
    hapus komponen yang tidak perlu

--- MainJpeg.c
+++ MainJpeg.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("--LSB Enkripsi-\n");    // Menampilkan judul program
+    printf("--LSB Enkripsi-\n");    
     printf("1: Enkrip code\n");
     printf("2: Dekrip code\n");
     printf("masukan pilihan : ");
@@ -12,27 +12,26 @@ int main() {
     char choice;
     scanf("%c", &choice);   // Membaca pilihan dari pengguna
 
-    if (choice == '1') {   										 // Jika pilihan adalah enkripsi
-        char src_image[100], dest_image[100], message[1000]; 	// Mendeklarasikan variabel untuk menyimpan path gambar sumber, path gambar tujuan, dan pesan yang akan disisipkan
-        printf("Masukkan gambar: ");   					 // Meminta pengguna memasukkan path gambar sumber
-        scanf("%s", src_image);									 // Membaca path gambar sumber
+    if (choice == '1') {   										
+        char src_image[100], dest_image[100], message[1000]; 	
+        printf("Masukkan gambar: ");   					 
+        scanf("%s", src_image);									 
         printf("Masukkan pesan yang ingin di sembunyikan : ");
-        getchar(); 												// Membersihkan karakter newline dari buffer
-        fgets(message, sizeof(message), stdin);   				// Membaca pesan yang akan disisipkan dari pengguna
-        message[strcspn(message, "\n")] = '\0'; 				// Menghapus karakter newline dari pesan
-        printf("Masukkan nama file yang telah di sisipkan pesan : ");   			// Meminta pengguna memasukkan path gambar tujuan
-        scanf("%s", dest_image);    							// Membaca path gambar tujuan
-        printf("Encoding...\n");    							// Menampilkan pesan bahwa proses enkripsi dimulai
-        encode(src_image, dest_image, message);    				// Memanggil fungsi untuk melakukan enkripsi
-    } else if (choice == '2') {    								// Jika pilihan adalah dekripsi
-        char src_image[100];    								// Mendeklarasikan variabel untuk menyimpan path gambar
-        printf("Masukkan gambar : ");    				// Meminta pengguna memasukkan path gambar
-        scanf("%s", src_image); 								// Membaca path gambar
-        printf("Decoding...\n");    							// Menampilkan pesan bahwa proses dekripsi dimulai
-        decode(src_image);  									// Memanggil fungsi untuk melakukan dekripsi
+        getchar(); 												
+        fgets(message, sizeof(message), stdin);   				
+        message[strcspn(message, "\n")] = '\0'; 				
+        printf("Masukkan nama file yang telah di sisipkan pesan : ");   			
+        scanf("%s", dest_image);    							
+        printf("Encoding...\n");    							
+        encode(src_image, dest_image, message);    				
+    } else if (choice == '2') {    								
+        char src_image[100];    								
+        printf("Masukkan gambar : ");    				
+        scanf("%s", src_image); 								
+        printf("Decoding...\n");    							
+        decode(src_image);  									
     } else {
-        printf("ERROR: Invalid option chosen\n"); 				// Menampilkan pesan kesalahan jika pilihan tidak valid
+        printf("ERROR: Invalid option chosen\n"); 				
     }
 
-    return 0;   // Mengembalikan nilai 0 yang menandakan keluar dari program tanpa kesalahan
-}
+    return 0;   
