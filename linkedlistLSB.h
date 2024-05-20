bba12be9801fafb091971925e48001a246a37644
AzkaDarajat <143983742+AzkaDarajat@users.noreply.github.com>
2024-05-20 07:49:01 +0700

    Header linkedlistLSB

---
+++ linkedlistLSB.h
@@ -0,0 +1,16 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node {
+    char data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+Node* createNode(char data);
+Node* insertEnd(Node* head, char data);
+void swapNodeData(Node* node1, Node* node2);
+void deleteNode(Node* prev, Node*curr);
+Node* insertAfter(Node* node, char data);
+
+#endif
