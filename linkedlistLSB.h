f05ca3ba4e18874ac7e5ddbd1027cc55de426b1c
pace <143985393+ilhamfs7@users.noreply.github.com>
2024-05-23 22:20:33 +0700

    Update linkedlistLSB.h

--- linkedlistLSB.h
+++ linkedlistLSB.h
@@ -1,5 +1,5 @@
-#ifndef LINKEDLISTLSB_H
-#define LINKEDLISTLSB_H
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
 typedef struct Node {
     char data;
@@ -10,7 +10,10 @@ typedef struct Node {
 Node* createNode(char data);
 Node* insertEnd(Node* head, char data);
 void swapNodeData(Node* node1, Node* node2);
-void deleteNode(Node* prev, Node*curr);
+void deleteNode(Node* prev, Node* curr);
 Node* insertAfter(Node* node, char data);
+void circularKanan(Node* head);
+void circularKiri(Node* head);
+void printList(Node* head);
 
 #endif
