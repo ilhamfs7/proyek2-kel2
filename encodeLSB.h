26a170811bedb639758d2ca62f8c51379f17b422
pace <143985393+ilhamfs7@users.noreply.github.com>
2024-05-23 22:22:49 +0700

    Update encodeLSB.h

--- encodeLSB.h
+++ encodeLSB.h
@@ -1,14 +1,8 @@
-#ifndef encodeLSB_h
-#define encodeLSB_h
+#ifndef ENCODELSB_H
+#define ENCODELSB_H
+#include <stdio.h>
 
-typedef struct Node {
-    char data;
-    struct Node* next;
-} Node;
-
-Node* createNode(char data);
-Node* insertEnd(Node* head, char data);
-void freeList(Node* head);
-void encode(const char* input_filename, const char* output_filename, const char* message);
+void encodeLSB(const char* input_filename, const char* output_filename, char* message);
+void readAndDeleteFile(char* filename, char* message, size_t message_size);
 
 #endif
