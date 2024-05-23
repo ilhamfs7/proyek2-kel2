dbe4410eb3fd66fc4b6ea6abf03cbab53bcf5313
ninoerico <158866190+ninoerico@users.noreply.github.com>
2024-05-23 14:58:01 +0700

    Update MainJpeg.cpp
    update dikit

--- MainJpeg.cpp
+++ MainJpeg.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
-
 #include "jpeg.h"
 #include <stdio.h>
 
 int main() {
-    printf("--LSB Enkripsi-\n");    
-    printf("1: Enkrip code\n");
-    printf("2: Dekrip code\n");
-    printf("masukan pilihan : ");
+    printf("--LSB Enkripsi jpeg--\n");    // Menampilkan judul program
+    printf("1: Enkripsi Pesan \n");
+    printf("2: Dekripsi Pesan \n");
+    printf("Masukkan pilihan: ");
     
     char choice;
-    scanf("%c", &choice);   // Membaca pilihan dari pengguna
-
-    if (choice == '1') {   										
+    scanf("%c", &choice);   
+    if (choice == '1') {   										 
         char src_image[100], dest_image[100], message[1000]; 	
-        printf("Masukkan gambar: ");   					 
-        scanf("%s", src_image);									 
-        printf("Masukkan pesan yang ingin di sembunyikan : ");
+        printf("Masukkan gambar: ");   					
+        scanf("%s", src_image);									
+        printf("Masukkan pesan yang ingin disembunyikan: ");
         getchar(); 												
         fgets(message, sizeof(message), stdin);   				
         message[strcspn(message, "\n")] = '\0'; 				
-        printf("Masukkan nama file yang telah di sisipkan pesan : ");   			
+        printf("Masukkan nama file yang telah disisipkan pesan: ");   			
         scanf("%s", dest_image);    							
         printf("Encoding...\n");    							
-        encode(src_image, dest_image, message);    				
+        encode_jpeg(src_image, dest_image, message);    				
     } else if (choice == '2') {    								
         char src_image[100];    								
-        printf("Masukkan gambar : ");    				
+        printf("Masukkan gambar: ");    			
         scanf("%s", src_image); 								
         printf("Decoding...\n");    							
-        decode(src_image);  									
+        decode_jpeg(src_image);  									
     } else {
-        printf("ERROR: Invalid option chosen\n"); 				
+        printf("ERROR: Pilihan Invalid \n"); 				
     }
 
     return 0;   
