238854c68b8f425b46accb8f925171f0ff2cc9ff
ninoerico <158866190+ninoerico@users.noreply.github.com>
2024-05-23 14:41:48 +0700

    Update Jpeg.h
    perbaruan

--- Jpeg.h
+++ Jpeg.h
@@ -10,17 +10,13 @@ typedef struct {
     uint8_t *buffer;        // Buffer untuk menyimpan data gambar
     size_t buffer_size;     // Ukuran buffer
 } ImageData;
-
 // Fungsi untuk membuka dan membaca file gambar
 void open_image(const char *filename, ImageData *image);
-
 // Fungsi untuk menutup buffer gambar
 void close_image(ImageData *image);
-
 // Fungsi untuk menyisipkan pesan ke dalam gambar
-void encode(const char *source_image, const char *dest_image, const char *message);
-
+void encode_jpeg(const char *source_image, const char *dest_image, const char *message);
 // Fungsi untuk mengekstrak pesan dari gambar
-void decode(const char *image_path);
+void decode_jpeg(const char *image_path);
 
-#endif /* IMAGE_H */
+#endif /* JPEG_H */
