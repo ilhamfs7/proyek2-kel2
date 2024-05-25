0d912223112a1844f89bb846d2bc2ca9c47e7088
nazla <158866001+nazlakayla@users.noreply.github.com>
2024-05-25 21:54:54 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "encodeLSB.h"
-#include "decodeLSB.h"
+#include "BMP.h"
 #include "rsa.h"
 #include "JPG.h"
 #include "validasi.h"
@@ -54,15 +53,14 @@ int main() {
 
         switch(choice) {
         	case 1:
-        	system("cls");
-        	printf("-------------------------------------------------------------\n");
-                printf("                    Enkripsi Pesan (RSA)                     \n");
-                printf("-------------------------------------------------------------\n\n");
-                
+        		system("cls");
+        		
                 do {
-			printf("Masukkan nama file untuk menyimpan hasil generate key (.txt): ");
-                	scanf("%s", key_filename);
                 	printf("-------------------------------------------------------------\n");
+                    printf("                       Enkripsi Pesan                        \n");
+                    printf("-------------------------------------------------------------\n\n");
+					printf("Masukkan nama file untuk menyimpan hasil generate key (.txt): ");
+                	scanf("%s", key_filename);
 					validasi = (!is_txt_file(key_filename));	
 				} while (validasi == 1);
 				    
@@ -136,40 +134,37 @@ int main() {
                 system("cls");
                 
                 while (1) {
-                	system("cls");
-                	printf("-------------------------------------------------------------\n");
-                	printf("                    Enkripsi Pesan (LSB)                     \n");
-                    printf("-------------------------------------------------------------\n\n");
-			        printf("Masukkan nama file gambar (.bmp / .jpg): ");
-			        scanf("%s", input_filename);
-			        getchar();
-			        
+                	do {
+                		system("cls");
+                		printf("-------------------------------------------------------------\n");
+                		printf("                    Enkripsi Pesan (LSB)                     \n");
+			        	printf("-------------------------------------------------------------\n");
+				        printf("\nMasukkan nama file gambar (.bmp / .jpg): ");
+				        scanf("%s", input_filename);
+				        while ((c = getchar()) != '\n' && c != EOF);
+				        
+				        // Cek apakah file ada
+						file = fopen(input_filename, "r");
+						if (file) {
+							// File ditemukan, tutup file dan keluar dari loop
+							fclose(file);
+							break;
+						} else {
+							// File tidak ditemukan, berikan peringatan dan minta input ulang
+							printf("File tidak ditemukan. Pastikan file tersebut ada di direktori dan coba lagi.\n\n");
+						}
+						system("pause");
+			    	} while (!file);
 			        // Get the file extension
 			        ext = strrchr(input_filename, '.');
 			        
 			        if (ext != NULL) {
 			            if (strcmp(ext, ".bmp") == 0) {
-			            	do {
-			            		printf("-------------------------------------------------------------\n");
-							    printf("Masukkan nama file yang berisi pesan (.txt): ");
-							    fgets(filename, sizeof(filename), stdin);
-							
-							    filename[strcspn(filename, "\n")] = 0;
-							
-							    char* extension = strrchr(filename, '.');
-							    if (extension == NULL || strcmp(extension, ".txt") != 0) {
-							    	printf("-------------------------------------------------------------\n");
-							        printf("Nama file harus memiliki ekstensi .txt\n");
-							    } else {
-							        break;
-							    }
-							} while (1);
-							
 			                do {
-							    printf("-------------------------------------------------------------\n");
-							    printf("Masukkan nama file untuk menyimpan salinan hasil enkripsi (.bmp): ");
+							    printf("----------------------------------------------------------\n");
+							    printf("Masukkan nama file untuk salinan (.bmp): ");
 							    scanf("%s", output_filename);
-							    getchar();
+							    while ((c = getchar()) != '\n' && c != EOF);
 							
 							    char* extension = strrchr(output_filename, '.');
 							    if (extension == NULL || strcmp(extension, ".bmp") != 0) {
@@ -178,6 +173,25 @@ int main() {
 							        break;
 							    }
 							} while (1);
+							
+							do {
+							    printf("----------------------------------------------------------\n");
+							    printf("Masukkan nama file yang akan dibaca (.txt): ");
+							    fgets(filename, sizeof(filename), stdin);
+							
+							    filename[strcspn(filename, "\n")] = 0;
+							
+							    file = fopen(filename, "r");
+								if (file) {
+									// File ditemukan, tutup file dan keluar dari loop
+									fclose(file);
+									break;
+								} else {
+									// File tidak ditemukan, berikan peringatan dan minta input ulang
+									printf("File tidak ditemukan. Pastikan file tersebut ada di direktori dan coba lagi.\n\n");
+								}
+								system("pause");
+							} while (!file);
 			                
 			                char* message = (char*)malloc(10000 * sizeof(char));
 			                
@@ -198,17 +212,22 @@ int main() {
 							    scanf("%s", message_file);
 							    getchar();
 							
-							    char* extension = strrchr(message_file, '.');
-							    if (extension == NULL || strcmp(extension, ".txt") != 0) {
-							        printf("Nama file harus memiliki ekstensi .txt\n");
-							    } else {
-							        break;
-							    }
-							} while (1);
+							    // Cek apakah file ada
+								file = fopen(message_file, "r");
+								if (file) {
+									// File ditemukan, tutup file dan keluar dari loop
+									fclose(file);
+									break;
+								} else {
+									// File tidak ditemukan, berikan peringatan dan minta input ulang
+									printf("File tidak ditemukan. Pastikan file tersebut ada di direktori dan coba lagi.\n\n");
+								}
+								system("pause");
+							} while (!file);
 							
 							do {
 							    printf("----------------------------------------------------------\n");
-							    printf("Masukkan nama file untuk menyimpan salinan hasil enkripsi (.jpg): ");
+							    printf("Masukkan nama salinan file (.jpg): ");
 							    scanf("%s", dest_image);
 							    getchar();
 							
@@ -220,17 +239,15 @@ int main() {
 							    }
 							} while (1);
 							
-			                printf("\n-------------------------------------\n");
+			                printf("-------------------------------------\n");
 			                printf("|             Encoding...           |\n");
 			                printf("-------------------------------------\n");
 			                encode_jpeg(input_filename, dest_image, message_file);
 			                break;
 			            } else {
-			            	printf("-------------------------------------------------------------\n");
 			                printf("Format file tidak dikenali. Harap masukkan file dengan format .bmp atau .jpg\n");
 			            }
 			        } else {
-			        	printf("-------------------------------------------------------------\n");
 			            printf("Tidak ada ekstensi file. Harap masukkan file dengan format .bmp atau .jpg\n");
 			        }
 			        system("pause");
@@ -243,10 +260,11 @@ int main() {
 						system("cls");
 						
 		            	printf("-------------------------------------------------------------\n");
-                		printf("                    Dekripsi Pesan (LSB)                     \n");
-                    	printf("-------------------------------------------------------------\n\n");
-				        printf("Masukkan nama file gambar (.bmp / .jpg): ");
+		                printf("                       Dekripsi Pesan                        \n");
+		                printf("-------------------------------------------------------------\n\n");
+				        printf("Masukkan nama file gambar(.bmp / .jpg): ");
 				        scanf("%s", input_filename);
+				        while ((c = getchar()) != '\n' && c != EOF);
 				
 				        // Cek apakah file ada
 				        file = fopen(input_filename, "r");
@@ -271,7 +289,7 @@ int main() {
 	                		decodeLSB(input_filename, &pesan, &benar);
 				        } else if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) {
 				            char src_image[100];
-					        printf("\n------------------------------------------\n");
+					        printf("------------------------------------------\n");
 	                        printf("|               Decoding...              |\n");  
 	                        printf("------------------------------------------\n");    							
 					        decode_jpeg(input_filename, &benar);
@@ -289,12 +307,22 @@ int main() {
                 do {
                 	printf("-------------------------------------------------------------\n");
                 	printf("                    Dekripsi Pesan (RSA)                     \n");
-                    printf("-------------------------------------------------------------\n\n");
+                	printf("---------------------------------------------------------------\n");
             		printf("Masukkan nama file input yang akan didekripsi (.txt): ");
                 	scanf("%s", input_filename);
-                	validasi = (!is_txt_file(input_filename));
                 	while ((c = getchar()) != '\n' && c != EOF);
-				} while (validasi == 1);
+                	// Cek apakah file ada
+					file = fopen(input_filename, "r");
+					if (file) {
+						// File ditemukan, tutup file dan keluar dari loop
+						fclose(file);
+						break;
+					} else {
+						// File tidak ditemukan, berikan peringatan dan minta input ulang
+						printf("File tidak ditemukan. Pastikan file tersebut ada di direktori dan coba lagi.\n\n");
+					}
+					system("pause");
+				} while (!file);
             	file_in = openFile(input_filename);
             	
             	do {
@@ -310,9 +338,19 @@ int main() {
                 	printf("---------------------------------------------------------------\n");
                 	printf("Masukkan nama file kunci (.txt) : ");
                 	scanf("%s", key_filename);
-                	validasi = (!is_txt_file(key_filename));
                 	while ((c = getchar()) != '\n' && c != EOF);
-				} while (validasi == 1);
+                	// Cek apakah file ada
+					file_key = fopen(key_filename, "r");
+					if (file_key) {
+						// File ditemukan, tutup file dan keluar dari loop
+						fclose(file_key);
+						break;
+					} else {
+						// File tidak ditemukan, berikan peringatan dan minta input ulang
+						printf("File tidak ditemukan. Pastikan file tersebut ada di direktori dan coba lagi.\n\n");
+					}
+					system("pause");
+				} while (!file_key);
 				file_key = openFile(key_filename);
                 
                 fscanf(file_key, "Public Key (e, n): (%*d, %*lld)\nPrivate Key (d, n): (%lld, %lld)", &d, &n);
@@ -330,7 +368,7 @@ int main() {
                 break;
             case 3:
                 exit = 1; 
-                printf("\n----------------------------------------------------------------------\n");
+                printf("----------------------------------------------------------------------\n");
                 printf("|             Terima kasih telah menggunakan program ini.            |\n");
                 printf("----------------------------------------------------------------------\n");
                 break;
