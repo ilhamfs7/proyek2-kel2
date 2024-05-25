62fd2e8c706fab364fa1ba72066b4fef21f8139b
AzkaDarajat <azkad123@gmail.com>
2024-05-25 20:58:10 +0700

    Pengahpusan file

--- decodeLSB.h
+++
@@ -1,7 +0,0 @@
-#ifndef DECODELSB_H
-#define DECODELSB_H
-
-void createMessageFile(const char* pesan);
-void decodeLSB(const char* input_filename, char** pesan);
-
-#endif
--- encodeLSB.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENCODELSB_H
-#define ENCODELSB_H
-#include <stdio.h>
-
-void encodeLSB(const char* input_filename, const char* output_filename, char* message);
-void readAndDeleteFile(char* filename, char* message, size_t message_size);
-
-#endif
