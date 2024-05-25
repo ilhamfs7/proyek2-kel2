7a71a330daff4c19ee67f2c2bd86ea5f62a6f0ad
AzkaDarajat <azkad123@gmail.com>
2024-05-25 20:57:21 +0700

    Perubahan encode decode LSB menjadi BMP.h dan BMP.c juga perubahan nama file linkedlist

---
+++ linkedlist.h
@@ -0,0 +1,20 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef struct Node {
+    char data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+Node* createNode(char data);
+Node* insertEnd(Node* head, char data);
+void swapNodeData(Node* node1, Node* node2);
+void deleteNode(Node* prev, Node* curr);
+Node* insertAfter(Node* node, char data);
+void circularKanan(Node* head);
+void circularKiri(Node* head);
+void printList(Node* head);
+int inputBener(int min, int max);
+
+#endif
